d204734f91448ab62c42dd59486b9faa4eba2697
Abu-Sahad <abusahad603@gmail.com>
2022-05-22 21:14:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Navbar from './Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Blogs from './Pages/Home/Blogs';
+import Login from './Pages/Login/Login';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="login" element={<Login />} />
+        <Route path="blogs" element={<Blogs />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
